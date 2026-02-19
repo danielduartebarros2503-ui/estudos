0726e4a1df25b06843ebf69ff625e41db6cb5d8a
Daniel <daniel.duartebarros2503@gmail.com>
2026-02-19 20:50:17 -0300

    :wq

---
+++ davi-julia-aura.py
@@ -0,0 +1,10 @@
+def sigma (julia, davi):
+    if julia > davi:
+        print("Julia tem mais aura que Davi")
+    elif julia < davi:
+        print("Davi tem mais aura que Julia")
+    else: 
+        print("Julia e Davi tem a mesma aura")
+        
+        
+sigma (5, 10)
---
+++ teste2.py
@@ -0,0 +1,19 @@
+def meb() :
+    print("dominadora mundial") 
+    
+def meb2() :
+    print("dominadora mundial de novo")
+    
+def brasil(bolsonaro, lula) :
+    if bolsonaro > lula :
+        print("Bolsonaro é o melhor presidente do Brasil")
+    elif lula > bolsonaro :
+        print("Lula é o melhor presidente do Brasil")
+    else : 
+        print("Bolsonaro e Lula são igualmente bons presidentes do Brasil")
+
+        
+
+brasil(10, 5)
+
+
